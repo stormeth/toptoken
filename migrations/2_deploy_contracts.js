57b1a38670c1e4b669aca06cb31c40794d264c16
Michael Angerman <stormasm@gmail.com>
2017-03-03 17:07:58 -0800

    MyAdvancedToken

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var MyAdvancedToken = artifacts.require("./MyAdvancedToken")
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(MyAdvancedToken)
 };
